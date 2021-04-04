5854122cc0b03d9fdf713985228a8419ccbf46e3
AnandGits <ganand28@gmail.com>
2021-04-03 18:31:15 -0700

    Test2

--- MySpringBoot/src/main/java/com/demo/springboot/controllers/ExampleController.java
+++ MySpringBoot/src/main/java/com/demo/springboot/controllers/ExampleController.java
@@ -1,7 +1,9 @@
 package com.demo.springboot.controllers;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,12 @@ public class ExampleController {
 	@GetMapping("/hello-world")
 	public ResponseEntity<String> get() {
 		return ResponseEntity.ok("Hello World!, Here I come");
+	}
+	
+	@GetMapping(path="/json-world", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> get1() {
+	//public @ResponseBody String getJSON(){
+		String s = "MyJSON, This is a test";
+		return ResponseEntity.ok(s);
 	}
 }
